c2bd2c2b5f539c7460912c5b35ca1d261addd5d2
lennartschering-hue <lennart.schering@gmail.com>
2026-06-07 14:16:28 +0200

    app.js

--- app.js
+++ app.js
@@ -1,32 +1,149 @@
-fetch("standings.json")
-    .then(response => response.json())
-    .then(data => {
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}
 
-        const tbody = document.querySelector("#leaderboard tbody");
+body{
+    font-family:'Inter',sans-serif;
+    background:#0F172A;
+    color:#F8FAFC;
+}
 
-        data.standings.forEach((player, index) => {
+.hero{
+    height:60vh;
 
-            const row = document.createElement("tr");
+    background:
+        linear-gradient(
+            rgba(0,0,0,.65),
+            rgba(0,0,0,.65)
+        ),
+        url("images/hero.jpg");
 
-            let place = index + 1;
+    background-size:cover;
+    background-position:center;
 
-            if (place === 1) {
-                place = "🥇";
-            } else if (place === 2) {
-                place = "🥈";
-            } else if (place === 3) {
-                place = "🥉";
-            }
+    display:flex;
+    align-items:center;
+    justify-content:center;
 
-            row.innerHTML = `
-                <td>${place}</td>
-                <td>${player.player}</td>
-                <td>${player.points}</td>
-            `;
+    text-align:center;
+}
 
-            tbody.appendChild(row);
-        });
-    })
-    .catch(error => {
-        console.error(error);
-    });
+.hero h1{
+    font-size:4rem;
+    margin-bottom:10px;
+}
+
+.hero p{
+    font-size:1.2rem;
+    color:#CBD5E1;
+}
+
+.hero-stats{
+    margin-top:40px;
+
+    display:flex;
+    gap:20px;
+    justify-content:center;
+    flex-wrap:wrap;
+}
+
+.hero-card{
+    background:#1E293B;
+    padding:20px;
+    min-width:140px;
+    border-radius:15px;
+}
+
+.hero-card span{
+    display:block;
+    font-size:1.5rem;
+    font-weight:700;
+}
+
+section{
+    padding:60px 10%;
+}
+
+h2{
+    margin-bottom:25px;
+}
+
+.podium-container{
+    display:grid;
+    grid-template-columns:repeat(3,1fr);
+    gap:20px;
+}
+
+.podium-card{
+    background:#1E293B;
+    border-radius:15px;
+    padding:25px;
+    text-align:center;
+}
+
+.podium-card h3{
+    margin-bottom:10px;
+}
+
+.stats-grid{
+    display:grid;
+    grid-template-columns:repeat(3,1fr);
+    gap:20px;
+}
+
+.stat-card{
+    background:#1E293B;
+    padding:25px;
+    border-radius:15px;
+    text-align:center;
+}
+
+.stat-card h3{
+    font-size:2rem;
+}
+
+table{
+    width:100%;
+    border-collapse:collapse;
+}
+
+thead{
+    background:#1E293B;
+}
+
+th,td{
+    padding:15px;
+}
+
+tbody tr{
+    border-bottom:1px solid #334155;
+}
+
+.results-grid{
+    display:grid;
+    grid-template-columns:repeat(auto-fill,minmax(250px,1fr));
+    gap:20px;
+}
+
+.result-card{
+    background:#1E293B;
+    padding:20px;
+    border-radius:15px;
+}
+
+@media(max-width:768px){
+
+    .hero h1{
+        font-size:2.3rem;
+    }
+
+    .podium-container{
+        grid-template-columns:1fr;
+    }
+
+    .stats-grid{
+        grid-template-columns:1fr;
+    }
+}
